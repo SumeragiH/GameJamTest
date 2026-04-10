2e127154a6963c15fdb21cc3d968dca76c5c6df0
yuidaisuki <1641636977@qq.com>
2026-04-10 16:08:58 +0800

    新增地块代码

---
+++ Assets/Scripts/View/PlotView.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlotView : MonoBehaviour
+{
+    // 产出
+    [field: SerializeField]public int grainOutput {get; private set;} = 0; // 粮食
+    [field: SerializeField]public int productivityOutput {get; private set;} = 0; // 生产力
+
+    // 地块状态
+    public bool isDetected {get; private set;} = false; // 是否被探明 
+
+    // 改良
+    // public List<Improvement> improvements {get; private set;} = new List<Improvement>(); // 已经建造的改良
+}
