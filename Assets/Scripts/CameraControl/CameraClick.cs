63388b0a0de0675348feced7bba904f257b07471
SumeragiH <1443490671@qq.com>
2026-04-10 20:12:51 +0800

    预设体添加了碰撞器，完成点击函数

--- Assets/Scripts/CameraControl/CameraClick.cs
+++ Assets/Scripts/CameraControl/CameraClick.cs
@@ -4,37 +4,52 @@ using UnityEngine;
 
 public class CameraClick : MonoBehaviour
 {
-    private GameObject Plot;//点击得到的地块
+    private GameObject Plot; // 点击得到的地块
 
     [Header("玩家是否可以点击")]
-    public bool isClick=true;
+    public bool isClick = true;
+
+    // 推荐：提前缓存射线层
+    private int plotLayer;
 
     private void Start()
     {
+        // 缓存Plot层
+        plotLayer = LayerMask.NameToLayer("Plot");
+
+        // 注册事件
         EventCenter.Instance.AddListener<GameObject>("点击地块", ClickPlot);
     }
+
     public void Update()
     {
-        if (!isClick)//如果玩家不能点击，直接返回
-            return;
-        if (Input.GetMouseButtonDown(0))//如果玩家点击了鼠标左键
+        if (!isClick) return;
+
+        // 鼠标左键点击
+        if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // 射线检测
+            if (Physics.Raycast(ray, out hit, 1000f))
             {
-                if(hit.collider.gameObject.layer==1<<LayerMask.NameToLayer("Plot"))
+                // 判断点击的是不是地块层
+                if (hit.collider.gameObject.layer == plotLayer)
                 {
-                    Debug.Log("点击了地块" + hit.collider.gameObject.name);
-                    EventCenter.Instance.EventTrigger("点击地块", hit.collider.gameObject);
+                    // 触发事件
+                    EventCenter.Instance.EventTrigger<GameObject>("点击地块", hit.collider.gameObject);
                 }
             }
-
         }
     }
 
+    /// <summary>
+    /// 事件接收方法
+    /// </summary>
     private void ClickPlot(GameObject plot)
     {
         Plot = plot;
-        Debug.Log("点击了地块" + Plot.name);
+        // 在这里可以添加点击地块后的逻辑，例如显示信息、移动相机等
     }
 }
