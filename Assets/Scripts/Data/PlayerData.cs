eb495f441e3ab7b1cf5aa68d9cafff01058beb91
SumeragiH <1443490671@qq.com>
2026-04-11 15:57:28 +0800

    角色系统框架

---
+++ Assets/Scripts/Data/PlayerData.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 玩家数据类：
+/// </summary>
+[System.Serializable]
+[CreateAssetMenu(fileName = "NewPlayerData", menuName = "Data/Player Data")]
+public class PlayerData : ScriptableObject
+{
+    public int playerID; // 玩家ID,唯一标识符
+    public string playerName; // 玩家名称，随机生成
+    public PlayerTypeEnum playerType; // 玩家类型
+
+    public int playerLevel; // 玩家等级
+    public float playerHealth; // 玩家健康值，范围0-100，影响玩家的生存能力
+    public float playerAtk; // 玩家攻击力，影响玩家的战斗能力
+    public float playerDef; // 玩家防御力，影响玩家的生存能力
+
+    public float MoodPoints; // 心情值，范围0-100，影响玩家的行为和产出效率
+    public float ExperiencePoints; // 经验值，玩家通过完成任务和活动获得，达到一定值后升级
+
+    public Sprite playerIcon; // 玩家形象
+    public override string ToString()
+    {
+        return $"{playerName} ({playerType})";
+    }
+}
---
+++ Assets/Scripts/Enum/PlayerTypeEnum.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerTypeEnum
+{
+    Fighter,// 战士：喜欢战斗，
+    Explorer,// 探险家：喜欢探索，
+    Socializer,// 社交者：喜欢社交，
+    None
+}
